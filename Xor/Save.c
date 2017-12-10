4489325745b5e11fea14e530ae733ce1f499fd1a
Victoria Krajcovicova <victoria.krajcovicocva@epita.fr>
2017-12-10 21:57:33 +0100

    print

--- Xor/Save.c
+++ Xor/Save.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-void Mat2File(char *path, void **Mat, size_t lines, size_t cols)
+void Mat2File(char *path, double **mat, size_t lines, size_t cols)
 {
 	FILE *file;
+	file = fopen(path, "w+");
+	//fputs("CACA BOUDIN PIPI\n",file);
 	
+	for(size_t i=0; i<lines;i++)
+	{
+		for(size_t j=0;j<cols;j++)
+			fprintf(file,"%f ",mat[i][j]);
+		fputc('\n',file);
+	}
+	fclose(file);	
 }
 
 int main()
 {
+	double **test = calloc(5,sizeof(double*));
+	for(size_t i = 0; i<5;i++)
+		test[i] = calloc(2,sizeof(double));
+	Mat2File("yop.txt",test,5,2);
+	
 	return 0;
 }
 
