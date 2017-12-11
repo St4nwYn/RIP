48eecd72f2dbd47895e7851d9c7ab531d1b6585d
Victoria Krajcovicova <victoria.krajcovicocva@epita.fr>
2017-12-11 22:34:17 +0100

    h

--- Treat_Image/treat_image.h
+++ Treat_Image/treat_image.h
@@ -8,20 +8,14 @@
 # include <SDL/SDL_image.h>
 # include <err.h>
 
-struct Matrix
-{
-	double **values;
-	size_t lines, cols;
-
-};
 //void SegmentationForInterface(SDL_Surface *img);
 void wait_for_keypressed(void);
-//struct Matrix *rogne(struct Matrix *mat)
-//struct Matrix *applysklt(struct Matrix *mat)
-//struct Matrix *sklt(struct Matrix *mat)
+struct Matrix *rogne(struct Matrix *mat)
+struct Matrix *applysklt(struct Matrix *mat)
+struct Matrix *sklt(struct Matrix *mat)
 SDL_Surface* Load_Image(char *path);
 SDL_Surface* display_image(SDL_Surface *img);
 void ToGrayScale(SDL_Surface *img);
 void Binarize(SDL_Surface* img);
-
+init(char* path);
 #endif
